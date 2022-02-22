5f6436857a10c30c9abea61a7581ea5690dffa38
Your Name <you@example.com>
2022-02-23 01:15:39 +0800

    feature

--- README.md
+++ README.md
@@ -1,11 +1,10 @@
-# Vue 3 + Typescript + Vite
+## 依赖包版本
 
-This template should help get you started developing with Vue 3 and Typescript in Vite. The template uses Vue 3 `<script setup>` SFCs, check out the [script setup docs](https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup) to learn more.
++ Vue ^3.2.25
++ Vite ^2.8.0
++ typescript ^4.5.4
 
-## Recommended IDE Setup
+## 简介
+vite 项目初始化时间为2022/2/19，所有依赖均为默认配置
 
-- [VSCode](https://code.visualstudio.com/) + [Volar](https://marketplace.visualstudio.com/items?itemName=johnsoncodehk.volar)
 
-## Type Support For `.vue` Imports in TS
-
-Since TypeScript cannot handle type information for `.vue` imports, they are shimmed to be a generic Vue component type by default. In most cases this is fine if you don't really care about component prop types outside of templates. However, if you wish to get actual prop types in `.vue` imports (for example to get props validation when using manual `h(...)` calls), you can enable Volar's `.vue` type support plugin by running `Volar: Switch TS Plugin on/off` from VSCode command palette.
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,36 +1,51 @@
-<script setup lang="ts">
-import { ref } from 'vue'
+<script lang="ts" setup>
+    import { ref, onMounted } from 'vue'
+    import { defineComponent } from 'vue'
 
-defineProps<{ msg: string }>()
+    defineProps<{ msg: string }>()
 
-const count = ref(0)
+    const count = ref(0)
+    function a(name) {
+
+    }
+    const c = 1
+    if (c==2) {
+        console.log(2)
+    }
+    onMounted(() => {
+        const e: HTMLElement | null = document.getElementById('a')
+        console.log(e)
+    })
+
+
+    type a = String
 </script>
 
 <template>
-  <h1>{{ msg }}</h1>
+    <h1 id="a">{{ msg }}</h1>
 
-  <p>
-    Recommended IDE setup:
-    <a href="https://code.visualstudio.com/" target="_blank">VSCode</a>
-    +
-    <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
-  </p>
+    <p>
+        Recommended IDE setup:
+        <a href="https://code.visualstudio.com/" target="_blank">VSCode</a>
+        +
+        <a href="https://github.com/johnsoncodehk/volar" target="_blank">Volar</a>
+    </p>
 
-  <p>See <code>README.md</code> for more information.</p>
+    <p>See <code>README.md</code> for more information.</p>
 
-  <p>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">
-      Vite Docs
-    </a>
-    |
-    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Docs</a>
-  </p>
+    <p>
+        <a href="https://vitejs.dev/guide/features.html" target="_blank">
+            Vite Docs
+        </a>
+        |
+        <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Docs</a>
+    </p>
 
-  <button type="button" @click="count++">count is: {{ count }}</button>
-  <p>
-    Edit
-    <code>components/HelloWorld.vue</code> to test hot module replacement.
-  </p>
+    <button type="button" @click="count++">count is: {{ count }}</button>
+    <p>
+        Edit
+        <code>components/HelloWorld.vue</code> to test hot module replacement.
+    </p>
 </template>
 
 <style scoped>
